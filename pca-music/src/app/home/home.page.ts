34fb996647184f2005f80b3891c2216bffecb49e
AngeloCianci <angelo.ciancie@pca.edu.co>
2025-07-08 20:51:28 -0500

    reconocimiento estructura app y prueba boton saludar

--- pca-music/src/app/home/home.page.ts
+++ pca-music/src/app/home/home.page.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { IonHeader, IonToolbar, IonTitle, IonContent} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
+  //variables,datos
+  mensaje = 'Hola mundo';
   constructor() {}
+  //funciones 
+  saludar(){
+    console.log(this.mensaje);
+  }
 }
